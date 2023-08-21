46c686f6923f321b9405fe6ee468bf05b4b8b2f4
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2023-08-20 21:57:00 -0300

    corrigir identação do arquivo

--- lib/database/connection.dart
+++ lib/database/connection.dart
@@ -1,33 +1,32 @@
-import 'package:autom_v3/utils/util.dart';
+import 'package:autom_v3/utils/environment_handler.dart';
 import 'package:postgres/postgres.dart';
 
 class Connection
 {
   static PostgreSQLConnection? _connection;
 
-  static Future<PostgreSQLConnection> getConnection() async 
-  {
-
-    String host = Util.getEnvirommentVariable( 'POSTGRES_DATABASE_HOST' ) ?? 'localhost';
-    String port = Util.getEnvirommentVariable( 'POSTGRES_DATABASE_PORT' ) ?? '5432';
-    String database = Util.getEnvirommentVariable( 'POSTGRES_DATABASE_NAME' ) ?? 'autom';
-    String username = Util.getEnvirommentVariable( 'POSTGRES_DATABASE_USERNAME' ) ?? 'autom';
-    String password = Util.getEnvirommentVariable( 'POSTGRES_DATABASE_PASSWORD' ) ?? 'autom';
-
-
-    if (_connection == null)
+    static Future<PostgreSQLConnection> getConnection() async 
     {
-      _connection = PostgreSQLConnection
-                    (
-                      host,
-                      int.parse( port ),
-                      database,
-                      username: username,
-                      password: password
-                    );
+        String host = EnvironmentHandler.get('POSTGRES_DATABASE_HOST');
+        String port = EnvironmentHandler.get('POSTGRES_DATABASE_PORT');
+        String database = EnvironmentHandler.get('POSTGRES_DATABASE_NAME');
+        String username = EnvironmentHandler.get('POSTGRES_DATABASE_USERNAME');
+        String password = EnvironmentHandler.get('POSTGRES_DATABASE_PASSWORD');
 
-      await _connection!.open();
+        if (_connection == null)
+        {
+            _connection = PostgreSQLConnection
+                        (
+                        host,
+                        int.parse( port ),
+                        database,
+                        username: username,
+                        password: password
+                        );
+
+            await _connection!.open();
+        }
+
+        return _connection!;
     }
-    return _connection!;
-  }
 }
