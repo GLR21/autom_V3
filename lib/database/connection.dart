a22c153b7564e5ea854948e23a61a9499d17db8a
SamuelUniverso <samuel.rockenbach@universo.univates.br>
2023-08-22 19:21:38 -0300

    card_65_abstrari_conexao_base_de_dados

--- lib/database/connection.dart
+++ lib/database/connection.dart
@@ -1,32 +1,45 @@
 import 'package:autom_v3/utils/environment_handler.dart';
-import 'package:postgres/postgres.dart';
+import 'package:fluent_query_builder/fluent_query_builder.dart';
 
 class Connection
 {
-  static PostgreSQLConnection? _connection;
-
-    static Future<PostgreSQLConnection> getConnection() async 
+    Future<DbLayer> _connect() async
     {
-        String host = EnvironmentHandler.get('POSTGRES_DATABASE_HOST');
-        String port = EnvironmentHandler.get('POSTGRES_DATABASE_PORT');
+        return await DbLayer(_dbInfo()).connect();
+    }
+
+    DBConnectionInfo _dbInfo()
+    {
         String database = EnvironmentHandler.get('POSTGRES_DATABASE_NAME');
         String username = EnvironmentHandler.get('POSTGRES_DATABASE_USERNAME');
         String password = EnvironmentHandler.get('POSTGRES_DATABASE_PASSWORD');
+        String host = EnvironmentHandler.get('POSTGRES_DATABASE_HOST');
+        String port = EnvironmentHandler.get('POSTGRES_DATABASE_PORT');
+        String charset = EnvironmentHandler.get('POSTGRES_DATABASE_CHARSET');
 
-        if (_connection == null)
+        return DBConnectionInfo(
+            host: host,
+            database: database,
+            driver: ConnectionDriver.pgsql,
+            port: int.parse(port),
+            username: username,
+            password: password,
+            charset: charset,
+            schemes: ['public'],
+        );
+    }
+
+    Future<DbLayer> getInstance()
+    {
+        final Future<DbLayer> conn;
+        try
         {
-            _connection = PostgreSQLConnection
-                        (
-                        host,
-                        int.parse( port ),
-                        database,
-                        username: username,
-                        password: password
-                        );
-
-            await _connection!.open();
+            conn = _connect();
         }
-
-        return _connection!;
+        catch(e, s)
+        {
+            throw Exception('fluent db error? $e $s');
+        }
+        return conn;
     }
 }
