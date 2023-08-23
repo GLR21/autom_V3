edb44ed4234527623070557acc4c0fc40de7d353
Gabriel Lange Ramos <gabriellange.ramos@gmail.com>
2023-08-22 22:20:36 -0300

    card_93: Criando conector e clase pai para models

--- lib/database/connection.dart
+++ lib/database/connection.dart
@@ -1,45 +1,18 @@
 import 'package:autom_v3/utils/environment_handler.dart';
-import 'package:fluent_query_builder/fluent_query_builder.dart';
+import 'package:postgres/postgres.dart';
 
 class Connection
 {
-    Future<DbLayer> _connect() async
-    {
-        return await DbLayer(_dbInfo()).connect();
-    }
 
-    DBConnectionInfo _dbInfo()
+    static getInstance()
     {
-        String database = EnvironmentHandler.get('POSTGRES_DATABASE_NAME');
-        String username = EnvironmentHandler.get('POSTGRES_DATABASE_USERNAME');
-        String password = EnvironmentHandler.get('POSTGRES_DATABASE_PASSWORD');
-        String host = EnvironmentHandler.get('POSTGRES_DATABASE_HOST');
-        String port = EnvironmentHandler.get('POSTGRES_DATABASE_PORT');
-        String charset = EnvironmentHandler.get('POSTGRES_DATABASE_CHARSET');
-
-        return DBConnectionInfo(
-            host: host,
-            database: database,
-            driver: ConnectionDriver.pgsql,
-            port: int.parse(port),
-            username: username,
-            password: password,
-            charset: charset,
-            schemes: ['public'],
+        return PostgreSQLConnection
+        (
+            EnvironmentHandler.get('POSTGRES_DATABASE_HOST'),
+            int.parse(EnvironmentHandler.get('POSTGRES_DATABASE_PORT')),
+            EnvironmentHandler.get('POSTGRES_DATABASE_NAME'),
+            username: EnvironmentHandler.get('POSTGRES_DATABASE_USERNAME'),
+            password: EnvironmentHandler.get('POSTGRES_DATABASE_PASSWORD'),
         );
-    }
-
-    Future<DbLayer> getInstance()
-    {
-        final Future<DbLayer> conn;
-        try
-        {
-            conn = _connect();
-        }
-        catch(e, s)
-        {
-            throw Exception('fluent db error? $e $s');
-        }
-        return conn;
     }
 }
