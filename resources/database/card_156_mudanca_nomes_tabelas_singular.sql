bcafe8c8eaeee03e885bbeefe89d430930eb1b30
Gabriel Lange Ramos <gabriellange.ramos@gmail.com>
2023-09-01 20:41:18 -0300

    card_156: Alterando nome das tabelas para singular

---
+++ resources/database/card_156_mudanca_nomes_tabelas_singular.sql
@@ -0,0 +1,4 @@
+ALTER TABLE at_marcas RENAME TO at_marca;
+ALTER TABLE at_pecas RENAME TO at_peca;
+ALTER TABLE at_pedidos RENAME TO at_pedido;
+ALTER TABLE at_pedidos_pecas RENAME TO at_pedido_peca;
