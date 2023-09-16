d82237ababa686caf9b7a558b07d4e9e8601ebb6
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2023-09-16 15:00:32 -0300

    Form básico para Estado

--- lib/view/estado/estado_view.dart
+++ lib/view/estado/estado_view.dart
@@ -1,26 +1,174 @@
 import 'package:autom_v3/view/components/navigation_panel.dart';
 import 'package:flutter/material.dart';
 
-class EstadoView extends StatelessWidget {
-    
+class EstadoView extends StatefulWidget
+{
     final int? id;
 
     const EstadoView(this.id, {super.key});
 
     @override
-    Widget build(BuildContext context) {
+    State<StatefulWidget> createState() => _EstadoView();
+}
 
-        String id = this.id.toString();
+class _EstadoView extends State<EstadoView>
+{
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-        Scaffold scaffold = Scaffold(
-            appBar: AppBar(
+    String? sigla;
+    String? nome;
+    String? codIbge;
+
+    Widget buildFieldSigla()
+    {
+        return TextFormField
+        (
+            decoration: const InputDecoration(label: Text('Sigla')),
+            validator: (String? value)
+            {
+                if(value!.isEmpty)
+                {
+                    return '"Sigla" é obrigatório';
+                }
+                return null;
+            },
+            onSaved: (newValue)
+            {
+                sigla = newValue;
+            },
+        );
+    }
+
+    Widget buildFieldNome()
+    {
+        return TextFormField
+        (
+            decoration: const InputDecoration(label: Text('Nome')),
+            validator: (String? value)
+            {
+                if(value!.isEmpty)
+                {
+                    return '"Nome" é obrigatório';
+                }
+                return null;
+            },
+            onSaved: (newValue)
+            {
+                nome = newValue;
+            },
+        );
+    }
+
+    Widget buildFieldCodIbge()
+    {
+        return TextFormField
+        (
+            decoration: const InputDecoration(label: Text('Nome')),
+            validator: (String? value)
+            {
+                if(value!.isEmpty)
+                {
+                    return '"Código IBGE" é obrigatório';
+                }
+                return null;
+            },
+            onSaved: (newValue)
+            {
+                codIbge = newValue;
+            },
+        );
+    }
+
+    @override
+    Widget build(BuildContext context)
+    {
+
+        // String id = .id.toString();
+
+        Scaffold scaffold = Scaffold
+        (
+            appBar: AppBar
+            (
                 title: const Text('Estados', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),),
                 backgroundColor: Colors.greenAccent,
             ),
             drawer: const NavigationPanel(),
-            body: Center(
-                child: Text('Estado View: $id')
-            ),
+            body: Scaffold
+            (
+                body: Wrap
+                (
+                    children:
+                    [
+                        Container
+                        (
+                            margin: const EdgeInsets.all(0),
+                            child: Form
+                            (
+                                key: formKey,
+                                child: Column
+                                (
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>
+                                    [
+                                        Padding
+                                        (
+                                            padding: const EdgeInsets.all(32),
+                                            child: Column
+                                            (
+                                                children:
+                                                [
+                                                    buildFieldSigla(),
+                                                    buildFieldNome(),
+                                                    buildFieldCodIbge()
+                                                ],                                            
+                                            ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(32),
+                                          child: Row
+                                          (
+                                              children:
+                                              [
+                                                  const Padding
+                                                  (
+                                                      padding: EdgeInsets.all(5)
+                                                  ),
+                                                  ElevatedButton
+                                                  (
+                                                      child: const Text
+                                                      (
+                                                          'Inserir',
+                                                          style: TextStyle(color: Colors.green),
+                                                      ),
+                                                      onPressed: ()
+                                                      {
+                                                          if(!formKey.currentState!.validate())
+                                                          {
+                                                              return;
+                                                          }
+                                        
+                                                          formKey.currentState!.save();
+                                        
+                                                          print(nome);
+                                                          print(sigla);
+                                                          print(codIbge);
+                                                      },
+                                                  ),
+                                              ],
+                                          ),
+                                        )
+                                    ],
+                                )
+                            ),
+                        ),
+                    ]
+                )
+            )
+            // body: Center
+            // (
+            //     child: Text('Estado View: $id')
+            // ),
         );
 
         return scaffold;
