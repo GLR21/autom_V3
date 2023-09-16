a8ff3b1e97f705520aab88c60bf08f360b83ac3d
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2023-09-16 14:03:41 -0300

    Form de busca básico para EstadoListview

--- lib/view/estado/estado_list_view.dart
+++ lib/view/estado/estado_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:autom_v3/classes/estado.dart';
 import 'package:autom_v3/models/estado_model.dart';
 import 'package:autom_v3/view/components/navigation_panel.dart';
+import 'package:autom_v3/view/estado/estado_view.dart';
 import 'package:flutter/material.dart';
 
 class EstadoListView extends StatefulWidget {
@@ -13,6 +14,50 @@ class EstadoListView extends StatefulWidget {
 
 class _EstadoListViewState extends State<EstadoListView>
 {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+    String? sigla = '';
+    String? nome = '';
+
+    Widget buildFieldSigla()
+    {
+        return TextFormField
+        (
+            decoration: const InputDecoration(label: Text('Sigla')),
+            validator: (String? value)
+            {
+                if(value!.isEmpty)
+                {
+                    return '"Sigla" é obrigatório';
+                }
+            },
+            onSaved: (newValue)
+            {
+                sigla = newValue;
+            },
+        );
+    }
+
+    Widget buildFieldNome()
+    {
+        return TextFormField
+        (
+            decoration: const InputDecoration(label: Text('Nome')),
+            validator: (String? value)
+            {
+                if(value!.isEmpty)
+                {
+                    return '"Nome" é obrigatório';
+                }
+            },
+            onSaved: (newValue)
+            {
+                nome = newValue;
+            },
+        );
+    }
+
+
     Future<List> getEstadoList() async
     {
         List list = await EstadoModel().selectAll();
@@ -35,49 +80,116 @@ class _EstadoListViewState extends State<EstadoListView>
             drawer: const NavigationPanel(),
             body: Scaffold
             (
-                body: Center
+                body: Wrap
                 (
-                    child: SingleChildScrollView
-                    (
-                        scrollDirection: Axis.vertical,
-                        child: FutureBuilder
+                    children:
+                    [
+                        Container
                         (
-                            future: getEstadoList(),
-                            builder: (context, snapshot)
-                            {
-                                if (snapshot.connectionState == ConnectionState.waiting)
-                                {
-                                    return const CircularProgressIndicator();
-                                }
-                                else if(snapshot.hasError)
-                                {
-                                    return Text('Error: ${snapshot.error}');
-                                }
-                                else
-                                {
-                                    var rows =  snapshot.data!;
-                                    var dts = DTS(rows);
-                                    int? rowPerPage = PaginatedDataTable.defaultRowsPerPage;
-                    
-                                    return PaginatedDataTable(
-                                        header: const Text('Estados'),
-                                        columns: const
-                                        [
-                                            DataColumn(label: Text('Sigla')),
-                                            DataColumn(label: Text('Nome')),
-                                        ],
-                                        source: dts,
-                                        onRowsPerPageChanged: (r)
-                                        {
-                                            rowPerPage = r;
-                                        },
-                                        rowsPerPage: rowPerPage,
-                                        
-                                    );
-                                }
-                            },
+                            margin: const EdgeInsets.all(0),
+                            child: Form
+                            (
+                                child: Column
+                                (
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>
+                                    [
+                                        Padding
+                                        (
+                                            padding: const EdgeInsets.all(32),
+                                            child: Column
+                                            (
+                                                children:
+                                                [
+                                                    buildFieldSigla(),
+                                                    buildFieldNome(),
+                                                    const SizedBox(height: 36)
+                                                ],                                            
+                                            ),
+                                        ),
+                                        Row
+                                        (
+                                            children:
+                                            [
+                                                ElevatedButton
+                                                (
+                                                    onPressed: ()
+                                                    {},
+                                                    child: const Text
+                                                    (
+                                                        'Buscar',
+                                                        style: TextStyle(color: Colors.green),
+                                                    ),
+                                                ),
+                                                const Padding
+                                                (
+                                                    padding: EdgeInsets.all(5)
+                                                ),
+                                                ElevatedButton
+                                                (
+                                                    onPressed: ()
+                                                    {
+                                                        Navigator.of(context).push
+                                                        (
+                                                            MaterialPageRoute
+                                                            (
+                                                                builder: (context) => const EstadoView(null),
+                                                            ),
+                                                        );
+                                                    },
+                                                    child: const Text
+                                                    (
+                                                        'Cadastrar',
+                                                        style: TextStyle(color: Colors.green),
+                                                    ),
+                                                ),
+                                            ],
+                                        )
+                                    ],
+                                )
+                            ),
                         ),
-                    ),
+                        Center
+                        (
+                            child: FutureBuilder
+                            (
+                                future: getEstadoList(),
+                                builder: (context, snapshot)
+                                {
+                                    if (snapshot.connectionState == ConnectionState.waiting)
+                                    {
+                                        return const CircularProgressIndicator();
+                                    }
+                                    else if(snapshot.hasError)
+                                    {
+                                        return Text('Error: ${snapshot.error}');
+                                    }
+                                    else
+                                    {
+                                        var rows =  snapshot.data!;
+                                        var dts = DTS(rows);
+                                        int? rowPerPage = PaginatedDataTable.defaultRowsPerPage;
+                        
+                                        return PaginatedDataTable(
+                                            header: const Text('Estados'),
+                                            columns: const
+                                            [
+                                                DataColumn(label: Text('Sigla')),
+                                                DataColumn(label: Text('Nome')),
+                                            ],
+                                            source: dts,
+                                            onRowsPerPageChanged: (r)
+                                            {
+                                                rowPerPage = r;
+                                            },
+                                            rowsPerPage: rowPerPage,
+                                            
+                                        );
+                                    }
+                                },
+                            ),
+                        ),
+                    ]
                 )
             )
         );
