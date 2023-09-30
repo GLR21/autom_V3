e392b52ce5ccb952b5c77d591fcd7deb23411ddd
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2023-09-30 19:25:02 -0300

    fix CidadeController

--- lib/controllers/cidade_controller.dart
+++ lib/controllers/cidade_controller.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: empty_catches
 
 import 'package:autom_v3/classes/cidade.dart';
-import 'package:autom_v3/classes/estado.dart';
 import 'package:autom_v3/models/cidade_model.dart';
-import 'package:autom_v3/models/estado_model.dart';
 
 class CidadeController
 {
