838ac9d8728985910bc0d19083b8c4133ee019b3
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2023-10-02 22:32:47 -0300

    Adicionar configurações de janela

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:autom_v3/view/main_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 Future<void> main() async
 {
     await dotenv.load(fileName: ".env");
+
     runApp(const MyApp());
+
+    doWhenWindowReady(()
+        {
+            const initialSize = Size(1440, 900);
+            appWindow.minSize = const Size(1280,720);
+            appWindow.size = initialSize;
+            appWindow.alignment = Alignment.center;
+            appWindow.show();
+        }
+    );
 }
 
 class MyApp extends StatelessWidget
