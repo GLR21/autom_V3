d87d63f36101b38bd1fed0c597a54e2ec29bf755
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2023-10-30 19:55:43 -0300

    Script para ignorar arquivos de build rastreados pelo git

---
+++ resources/git/git-assume-unchanged.sh
@@ -0,0 +1,6 @@
+git update-index --assume-unchanged linux/flutter/generated_plugin_registrant.cc
+git update-index --assume-unchanged linux/flutter/generated_plugins.cmake
+git update-index --assume-unchanged macos/Flutter/GeneratedPluginRegistrant.swift
+git update-index --assume-unchanged windows/flutter/generated_plugin_registrant.cc
+git update-index --assume-unchanged windows/flutter/generated_plugins.cmake
+git update-index --assume-unchanged windows/runner/flutter_window.cpp
